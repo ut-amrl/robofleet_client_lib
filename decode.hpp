a48657331e663abbdf0058645ebf44045bae8665
nickmac459 <nrmachak@gmail.com>
2021-04-23 22:59:01 +0000

    Manthan's changes to robofleet_client_lib

--- decode.hpp
+++ decode.hpp
@@ -43,3 +43,16 @@ RobotStatus decode(
   dst.status = src->status()->str();
   return dst;
 }
+
+template <>
+struct flatbuffers_type_for<ElevatorCommand> {
+  typedef fb::amrl_msgs::ElevatorCommand type;
+};
+template <>
+ElevatorCommand decode(
+    const fb::amrl_msgs::ElevatorCommand* const src) {
+  ElevatorCommand dst;
+  dst.floor_cmd = src->floor_cmd();
+  dst.hold_door = src->hold_door();
+  return dst;
+}
--- encode.hpp
+++ encode.hpp
@@ -65,3 +65,13 @@ flatbuffers::uoffset_t encode(
              fbb, metadata, msg.topic_regex.c_str(), msg.action)
       .o;
 }
+
+
+// amrl_msgs/ElevatorStatus
+template <>
+flatbuffers::uoffset_t encode(
+    FBB& fbb, const ElevatorStatus& msg, const MetadataOffset& metadata) {
+  return fb::amrl_msgs::CreateElevatorStatus(
+             fbb, metadata, msg.floor, msg.door)
+      .o;
+}
