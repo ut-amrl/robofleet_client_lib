c05918ce916f1659bd1ecba085408e04a2edf159
maxsvetlik <maxsvetlik@gmail.com>
2021-07-14 10:04:16 -0500

    expands common ros message definitions

--- message_structs.h
+++ message_structs.h
@@ -11,6 +11,7 @@ struct RobotLocation {
 	std::string frame;
 	float x;
 	float y;
+	float z;
 	float theta;
 };
 
@@ -19,4 +20,77 @@ struct RobotStatus {
 	bool is_ok;
 	float battery_level;
 	std::string location;
+};
+
+
+/*
+ * ROS message clones
+ */
+// std_msgs
+struct Header {
+	std::string frame_id;
+	uint32_t seq;
+};
+
+// geometry_msgs
+struct Point {
+	float x;
+	float y;
+	float z;
+};
+
+struct Quaternion {
+	float x;
+	float y;
+	float z;
+	float w;
+};
+
+struct Vector3 {
+	double x;
+	double y;
+	double z;
+};
+
+struct Pose {
+	Point point;
+	Quaternion quaternion;
+};
+
+struct PoseStamped {
+	Header header;
+	Pose pose;
+};
+
+struct PoseWithCovariance {
+	Pose pose;
+	double covariance[36];
+};
+
+struct PoseWithCovarianceStamped {
+	Header header;
+	PoseWithCovariance pose;
+};
+
+struct Twist {
+	Vector3 linear;
+	Vector3 angular;
+};
+
+struct TwistWithCovariance {
+	Twist twist;
+	double covariance[36];
+};
+
+struct TwistWithCovarianceStamped {
+	Header header;
+	TwistWithCovariance twist;
+};
+
+// nav_msgs
+struct Odometry {
+	Header header;
+	std::string child_frame_id;
+	PoseWithCovariance pose;
+	TwistWithCovariance twist;
 };
